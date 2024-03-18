c557ee6e7541cae394eb4ab620da14b11789f672
gianpierobozza <gianpiero.bozza@gmail.com>
2024-03-18 09:13:31 +0100

    fixed middleware with missing files

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -24,6 +24,7 @@ export function middleware(request: NextRequest) {
   if (
     [
       "/manifest.json",
+      "/favicon.ico",
       // Your other files in `public`
     ].includes(pathname)
   ) return;
